9186c2907a6d680b52f8c8d1b59ed8908d886316
Jan Clemens <clemensjan@googlemail.com>
2016-11-13 09:19:39 -0500

    Create cv2to3.py

---
+++ cv2to3.py
@@ -0,0 +1,19 @@
+try: # import opencv if necessary
+	  cv2
+except NameError:
+	  import cv2
+
+def isCV2():
+    return cv2.__version__[0] == '2'
+
+def isCV3():
+    return cv2.__version__[0] == '3'
+
+# Compatibility fix for OpenCV < 3.0
+if (cv2.__version__[0] == '2') or (not cv2.__version__[0] == '3'):
+    # map all cv2.cv.CV_* to cv2.*
+    for key, value in cv2.cv.__dict__.items():
+        if key.startswith('CV_'):
+            setattr(cv2, key[3:], value)
+    # fix some specific changes
+    cv2.VideoWriter_fourcc = cv2.cv.CV_FOURCC
